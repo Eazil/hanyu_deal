ff4a7360f58076dbdf577ee1905f92b8a64d3b96
Eaziln <1242450854@qq.com>
2018-03-31 12:03:49 +0800

    标点句的个数及平均次数

--- nanshou.py
+++ nanshou.py
@@ -2,25 +2,26 @@
 #
 import re
 import codecs
+import os
 
 #
-file = codecs.open("./hanyu_output_dp.txt", "r", "utf-8")
-string = file.read()
-file.close()
-# print(string)
-#
-juzi = re.findall('.*?\r\n\r\n',string,re.S)
-
+# file = codecs.open("./hanyu_output_dp.txt", "r", "utf-8")
+# string = file.read()
+# file.close()
+# # print(string)
 # #
-n = 1
-for x in juzi:
-    number = str(n).zfill(5)
-    s = 'S'+number+'\r\n'+x
-    print(s)
-    file1 = codecs.open("./hanyu_output_dp_id.txt", "a", "utf-8")
-    file1.write(s)
-    file1.close()
-    n +=1
+# juzi = re.findall('.*?\r\n\r\n',string,re.S)
+#
+# # #
+# n = 1
+# for x in juzi:
+#     number = str(n).zfill(5)
+#     s = 'S'+number+'\r\n'+x
+#     print(s)
+#     file1 = codecs.open("./hanyu_output_dp_id.txt", "a", "utf-8")
+#     file1.write(s)
+#     file1.close()
+#     n +=1
 # s = juzi[0]
 # # file1 = codecs.open("./hanyu_qChong.txt", "a", "utf-8")
 # # file1.write(s)
@@ -101,3 +102,37 @@ for x in juzi:
 #     file2.close()
 #
 # pynlpir.close()
+
+#计算标点句的个数sentence及标点句平均词数 word_v
+def panduan(word):
+    biaodian_list = ['，', '。', '；', '', '：', '？', '！', '——', '……']
+    for biaodian in biaodian_list:
+        if word == biaodian:
+            return True
+    return False
+year=str(2002)
+input="H:\\cws\\"+year+"_cws\\version_3_cws"
+# total=''
+for parents, folders, filenames in os.walk(input):
+    for filename in filenames:
+        file = os.path.join(input,filename)
+        # print(file)
+        file1 = codecs.open(file, "r", "utf-8")
+        s = file1.read()
+        file1.close()
+        word_list = s.split(' ')
+        biaodian_list = ['，','。','；','','：','？','！','——','……']
+        n = 0
+        for word in word_list:
+            if panduan(word):
+                n+=1
+                # print(word)
+        sentence = n
+        word_v = round((len(word_list) - sentence) / sentence,1)
+        total = file+"\r\n"+"CL-107-V3|"+str(sentence)+"\r\nCL-108-V3|"+str(word_v)+"\r\n\r\n"
+        file1 = codecs.open('./'+year+"_CL-107and108.txt", "a", "utf-8")
+        file1.write(total)
+        file1.close()
+
+
+
--- test.py
+++ test.py
@@ -1,80 +1,80 @@
-# -*- coding:utf-8 -*-
-
-import re
-import time
-from selenium import webdriver
-import codecs
-
-browser = webdriver.Chrome()
-browser.get('http://www.aihanyu.org/syntax.aspx')
-yfjg = ['固定结构','介词及介词结构','特殊句式','复句']
-for f in range(1,5):
-    button = browser.find_element_by_css_selector('input#ctl00_MainContent_Button'+str(f))
-    button.click()
-    for d in range(1,3):
-        yf = browser.find_elements_by_css_selector('div#ctl00_MainContent_Panel'+str(d)+' div a')
-        for i in yf:
-            url = i.get_attribute('href')
-            yuFaDian = i.text
-            driver = webdriver.Chrome()
-            driver.get(url)
-            click_one = driver.find_elements_by_css_selector('span.underline')
-            # print(click_one.text)
-            bianbie = False
-            lengh = 0
-            if len(click_one)>=2 and click_one[1].text == '形式类别（点击）':
-                click_one[1].click()
-                time.sleep(3)
-                yufaJG_1=driver.find_element_by_id('2')
-                yufaJG_2 = yufaJG_1.find_elements_by_css_selector('a')
-                lengh = len(yufaJG_2)
-                bianbie = True
-                # for k in yufaJG_2:
-                #     yufajg = k.text
-                #     total = yfjg[f - 1] + '|' + yuFaDian + '|' + yufajg + '|' + url+'\r\n'
-                #     print(total)
-                #     file = codecs.open("./hanyu_c.txt", "a", "utf-8")
-                #     file.write(total)
-                #     file.close()
-            else:
-                click_two = driver.find_elements_by_css_selector('div#ctl00_MainContent_Panel1 a')[2]
-                if click_two.text == '语义信息':
-                    click_two.click()
-                    time.sleep(3)
-                    yufaJG_1 = driver.find_element_by_id('2')
-                    yufaJG_2 = yufaJG_1.find_elements_by_css_selector('a')
-                    lengh = len(yufaJG_2)
-                    # for k in yufaJG_2:
-                    #     yufajg = k.text
-                    #     total = yfjg[f-1]+'|'+yuFaDian+'|'+yufajg+'|'+url+'\r\n'
-                    #     print(total)
-                    #     file = codecs.open("./hanyu_c.txt", "a", "utf-8")
-                    #     file.write(total)
-                    #     file.close()
-            total = ''
-            for l in range(0,lengh):
-                yufaJG_1 = driver.find_element_by_id('2')
-                yufaJG_2 = yufaJG_1.find_elements_by_css_selector('a')
-                yufajg = yufaJG_2[l].text
-                yufaJG_2[l].click()
-                record = driver.find_element_by_id('ctl00_MainContent_lbRecord')
-                record_n = record.text
-                total = yfjg[f-1]+'|'+yuFaDian+'|'+yufajg+'|'+url+'|'+record_n+'\r\n'
-                if bianbie:
-                    click_one = driver.find_elements_by_css_selector('span.underline')
-                    click_one[1].click()
-                    time.sleep(3)
-                else:
-                    click_two = driver.find_elements_by_css_selector('div#ctl00_MainContent_Panel1 a')[2]
-                    click_two.click()
-                    time.sleep(3)
-                file = codecs.open("./hanyu_number.txt", "a", "utf-8")
-                print(total)
-                file.write(total)
-                file.close()
-
-
-            driver.close()
-
-
-browser.close()
+# # -*- coding:utf-8 -*-
+#
+# import re
+# import time
+# from selenium import webdriver
+# import codecs
+#
+# browser = webdriver.Chrome()
+# browser.get('http://www.aihanyu.org/syntax.aspx')
+# yfjg = ['固定结构','介词及介词结构','特殊句式','复句']
+# for f in range(1,5):
+#     button = browser.find_element_by_css_selector('input#ctl00_MainContent_Button'+str(f))
+#     button.click()
+#     for d in range(1,3):
+#         yf = browser.find_elements_by_css_selector('div#ctl00_MainContent_Panel'+str(d)+' div a')
+#         for i in yf:
+#             url = i.get_attribute('href')
+#             yuFaDian = i.text
+#             driver = webdriver.Chrome()
+#             driver.get(url)
+#             click_one = driver.find_elements_by_css_selector('span.underline')
+#             # print(click_one.text)
+#             bianbie = False
+#             lengh = 0
+#             if len(click_one)>=2 and click_one[1].text == '形式类别（点击）':
+#                 click_one[1].click()
+#                 time.sleep(3)
+#                 yufaJG_1=driver.find_element_by_id('2')
+#                 yufaJG_2 = yufaJG_1.find_elements_by_css_selector('a')
+#                 lengh = len(yufaJG_2)
+#                 bianbie = True
+#                 # for k in yufaJG_2:
+#                 #     yufajg = k.text
+#                 #     total = yfjg[f - 1] + '|' + yuFaDian + '|' + yufajg + '|' + url+'\r\n'
+#                 #     print(total)
+#                 #     file = codecs.open("./hanyu_c.txt", "a", "utf-8")
+#                 #     file.write(total)
+#                 #     file.close()
+#             else:
+#                 click_two = driver.find_elements_by_css_selector('div#ctl00_MainContent_Panel1 a')[2]
+#                 if click_two.text == '语义信息':
+#                     click_two.click()
+#                     time.sleep(3)
+#                     yufaJG_1 = driver.find_element_by_id('2')
+#                     yufaJG_2 = yufaJG_1.find_elements_by_css_selector('a')
+#                     lengh = len(yufaJG_2)
+#                     # for k in yufaJG_2:
+#                     #     yufajg = k.text
+#                     #     total = yfjg[f-1]+'|'+yuFaDian+'|'+yufajg+'|'+url+'\r\n'
+#                     #     print(total)
+#                     #     file = codecs.open("./hanyu_c.txt", "a", "utf-8")
+#                     #     file.write(total)
+#                     #     file.close()
+#             total = ''
+#             for l in range(0,lengh):
+#                 yufaJG_1 = driver.find_element_by_id('2')
+#                 yufaJG_2 = yufaJG_1.find_elements_by_css_selector('a')
+#                 yufajg = yufaJG_2[l].text
+#                 yufaJG_2[l].click()
+#                 record = driver.find_element_by_id('ctl00_MainContent_lbRecord')
+#                 record_n = record.text
+#                 total = yfjg[f-1]+'|'+yuFaDian+'|'+yufajg+'|'+url+'|'+record_n+'\r\n'
+#                 if bianbie:
+#                     click_one = driver.find_elements_by_css_selector('span.underline')
+#                     click_one[1].click()
+#                     time.sleep(3)
+#                 else:
+#                     click_two = driver.find_elements_by_css_selector('div#ctl00_MainContent_Panel1 a')[2]
+#                     click_two.click()
+#                     time.sleep(3)
+#                 file = codecs.open("./hanyu_number.txt", "a", "utf-8")
+#                 print(total)
+#                 file.write(total)
+#                 file.close()
+#
+#
+#             driver.close()
+#
+#
+# browser.close()
