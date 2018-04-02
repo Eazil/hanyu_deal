1007d830585ae8c875ff6ccef71076754776a58b
Eaziln <1242450854@qq.com>
2018-04-02 15:09:57 +0800

    句子id

--- nanshou.py
+++ nanshou.py
@@ -104,35 +104,65 @@ import os
 # pynlpir.close()
 
 #计算标点句的个数sentence及标点句平均词数 word_v
-def panduan(word):
-    biaodian_list = ['，', '。', '；', '', '：', '？', '！', '——', '……']
-    for biaodian in biaodian_list:
-        if word == biaodian:
-            return True
-    return False
-year=str(2002)
-input="H:\\cws\\"+year+"_cws\\version_3_cws"
-# total=''
-for parents, folders, filenames in os.walk(input):
-    for filename in filenames:
-        file = os.path.join(input,filename)
-        # print(file)
-        file1 = codecs.open(file, "r", "utf-8")
-        s = file1.read()
-        file1.close()
-        word_list = s.split(' ')
-        biaodian_list = ['，','。','；','','：','？','！','——','……']
-        n = 0
-        for word in word_list:
-            if panduan(word):
-                n+=1
-                # print(word)
-        sentence = n
-        word_v = round((len(word_list) - sentence) / sentence,1)
-        total = file+"\r\n"+"CL-107-V3|"+str(sentence)+"\r\nCL-108-V3|"+str(word_v)+"\r\n\r\n"
-        file1 = codecs.open('./'+year+"_CL-107and108.txt", "a", "utf-8")
-        file1.write(total)
-        file1.close()
+
+# def panduan(word):
+#     biaodian_list = ['，', '。', '；', '', '：', '？', '！', '——', '……']
+#     for biaodian in biaodian_list:
+#         if word == biaodian:
+#             return True
+#     return False
+# year=str(2002)
+# input="H:\\cws\\"+year+"_cws\\version_3_cws"
+# # total=''
+# for parents, folders, filenames in os.walk(input):
+#     for filename in filenames:
+#         file = os.path.join(input,filename)
+#         # print(file)
+#         file1 = codecs.open(file, "r", "utf-8")
+#         s = file1.read()
+#         file1.close()
+#         word_list = s.split(' ')
+#         biaodian_list = ['，','。','；','','：','？','！','——','……']
+#         n = 0
+#         for word in word_list:
+#             if panduan(word):
+#                 n+=1
+#                 # print(word)
+#         sentence = n
+#         word_v = round((len(word_list) - sentence) / sentence,1)
+#         total = file+"\r\n"+"CL-107-V3|"+str(sentence)+"\r\nCL-108-V3|"+str(word_v)+"\r\n\r\n"
+#         file1 = codecs.open('./'+year+"_CL-107and108.txt", "a", "utf-8")
+#         file1.write(total)
+#         file1.close()
+#
+#
+
+#给与新工具的中间文件id
+file1 = codecs.open('./hanyu_unique_output.txt', "r", "utf-8")
+s = file1.read()
+file1.close()
+
+file2= codecs.open('G:\code\汉语\hanyu_unique_output.txt.parsed.txt', "r", "utf-8")
+s1 = file2.read()
+file2.close()
+
+s_list = s1.split('\r\n\r\n')
+
+for i in range(1,len(s_list)):
+    s2 = s_list[i]
 
 
-
+    r = re.findall('(.*?)\t.*?\t.*?\t.*?', s2)
+    string = ''
+    for word in r:
+        string += word
+    # string = string.replace('(', '\(')
+    # string = string.replace(')', '\)')
+    # string = string.replace('.', '\.')
+    # string = string.replace('?', '\?')
+    if string in s:
+        # number = re.search('(.*?)\|'+string,s)
+        print(string)
+        # print(number[1])
+    else:
+        print('False:'+s2)
