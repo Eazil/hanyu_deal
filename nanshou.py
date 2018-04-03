36638d1376d4ede145cb829a2b4639c19780aca6
Eaziln <1242450854@qq.com>
2018-04-03 09:37:15 +0800

    提取niuparser和新工具对应句子处理结果

--- nanshou.py
+++ nanshou.py
@@ -137,32 +137,76 @@ import os
 #
 #
 
-#给与新工具的中间文件id
-file1 = codecs.open('./hanyu_unique_output.txt', "r", "utf-8")
+# #给与新工具的中间文件id
+# file1 = codecs.open('./hanyu_unique_output.txt', "r", "utf-8")
+# s = file1.read()
+# file1.close()
+# s = s.replace(' ','')
+#
+# file2= codecs.open('G:\code\汉语\hanyu_unique_output.txt.parsed.txt', "r", "utf-8")
+# s1 = file2.read()
+# file2.close()
+#
+# s_list = s1.split('\r\n\r\n')
+# n = ''
+# x = ''
+# for i in range(1,len(s_list)):
+#     s2 = s_list[i]
+#
+#
+#     r = re.findall('(.*?)\t.*?\t.*?\t.*?', s2)
+#     string = ''
+#     for word in r:
+#         string += word
+#     string = string.replace('(', '\(')
+#     string = string.replace(')', '\)')
+#     string = string.replace('.', '\.')
+#     string = string.replace('?', '\?')
+#     # if string in s:
+#     number = re.search('(.*?)\|'+string+'\r\n',s)
+#
+#     # print(string)
+#     if number==None:
+#
+#         ss = 'S'+str(int(n.strip('S'))+1).zfill(5)
+#
+#         if ss != x:
+#             to = ss+'\r\n'+s2+'\r\n\r\n'
+#             file1 = codecs.open('./hanyu_新工具.txt', "a", "utf-8")
+#             file1.write(to)
+#             file1.close()
+#         else:
+#             to= s2+'\r\n\r\n '
+#             file1 = codecs.open('./hanyu_新工具.txt', "a", "utf-8")
+#             file1.write(to)
+#             file1.close()
+#         x = ss
+#
+#         # if n != x:
+#         #     print(string)
+#         #     print('False:'+n)
+#         # x = n
+#
+#     else:
+#         n = number[1]
+#
+#     # else:
+#     #     print('False:'+string)
+#提取niuparser子句
+file1 = codecs.open('./hanyu_新工具.txt', "r", "utf-8")
 s = file1.read()
 file1.close()
 
-file2= codecs.open('G:\code\汉语\hanyu_unique_output.txt.parsed.txt', "r", "utf-8")
-s1 = file2.read()
-file2.close()
+list = re.findall("(S\d{5}\r\n)",s)
+print(len(list))
+file1 = codecs.open('./hanyu_output_dp_id.txt', "r", "utf-8")
+s1 = file1.read()
+file1.close()
+# for i in list:
+#     x = re.search(i+'.*?\r\n\r\n',s1,re.S)
+#     print(x[0])
+#     file1 = codecs.open('./hanyu_dp.txt', "a", "utf-8")
+#     file1.write(x[0])
+#     file1.close()
 
-s_list = s1.split('\r\n\r\n')
 
-for i in range(1,len(s_list)):
-    s2 = s_list[i]
-
-
-    r = re.findall('(.*?)\t.*?\t.*?\t.*?', s2)
-    string = ''
-    for word in r:
-        string += word
-    # string = string.replace('(', '\(')
-    # string = string.replace(')', '\)')
-    # string = string.replace('.', '\.')
-    # string = string.replace('?', '\?')
-    if string in s:
-        # number = re.search('(.*?)\|'+string,s)
-        print(string)
-        # print(number[1])
-    else:
-        print('False:'+s2)
